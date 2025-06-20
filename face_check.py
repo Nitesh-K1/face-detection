a8666673c32483e01e32e6a65140ad442e0dd2a3
Nitesh Kafle <niteshkafle23@gmail.com>
2025-06-20 20:29:23 +0545

    changed the logic to determine the face of the image

--- face_check.py
+++ face_check.py
@@ -1,11 +1,9 @@
 import face_recognition
-known_image=face_recognition.load_image_file("images/known.jpg")
-known_encoding=face_recognition.face_encodings(known_image)[0]
-
-test_image=face_recognition.load_image_file("images/test.jpg")
-test_encoding=face_recognition.face_encodings(test_image)[0]
-results=face_recognition.compare_faces([known_encoding], test_encoding)
-if results[0]:
-    print("its a match same person.")
-else:
-    print("not a match. different person")
+known_image = face_recognition.load_image_file("images/known.jpg")
+test_image = face_recognition.load_image_file("images/test.jpg")
+known_encoding = face_recognition.face_encodings(known_image)[0]
+test_encoding = face_recognition.face_encodings(test_image)[0]
+results = face_recognition.compare_faces([known_encoding], test_encoding)
+distance = face_recognition.face_distance([known_encoding], test_encoding)[0]
+print(f"Face Match: {results[0]}")
+print(f"Confidence Score (lower is better): {distance:.4f}")
